3cd9626e8bc009fa1cc74f15b6b0bd6601c08139
davidemily <daeth5@mail.missouri.edu>
2019-07-28 12:51:32 -0500

    edited seo.js to remove references to twitter

--- src/components/seo.js
+++ src/components/seo.js
@@ -46,27 +46,7 @@ function SEO({ description, lang, meta, title }) {
         {
           property: `og:description`,
           content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
+        }
       ].concat(meta)}
     />
   )
